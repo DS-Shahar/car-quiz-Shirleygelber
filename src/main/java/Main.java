434bc8a2a5d188d6abd60fcc4d54dde088085691
Shirleygelber <shir2gel@gmail.com>
2024-10-06 12:34:34 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package carInfo;
+
+public class Main {
+public static void main(String[]args) {
+public static int LegalCities (CameraInfo [] cameras) {
+
+	boolean flag = true;
+	int count = 0;
+	for (int i = 0; i<100 ;i++){ 
+		for(int j=0; j< cameras.length; j++){
+			if (cameras[j].getCities == i ) {
+				if (cameras[j]. allGood == true) {
+					flag=true;
+				}
+				 else {
+					flag = false;
+				 }
+					if (flag== true){
+
+					count += 1;
+					}
+			}
+		}
+	}
+	return count;
+
 }
+}
+	
